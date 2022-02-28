c0d57de7bc8ac2f944f0bd136910afebdb88d9f4
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2022-02-27 23:02:18 -0500

    entité clients et relation facturer

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,12 +1,23 @@
+create table if not exists Clients
+(
+    id_client  char(36)    not null,
+    nom       varchar(20) not null,
+    prenom    varchar(20),
+    email     varchar(20),
+    adresse   varchar(50),
+    telephone char(11),
+    PRIMARY KEY (id_client)
+);
+
 CREATE TABLE Produits(
-    id_produit char(12) not null,
+    id_produit char(36) not null, #le uuid de python retourne 36 caracteres donc j'ai mis à 36
     prix double,
     quantity integer,
     PRIMARY KEY (id_produit)
 );
 
 CREATE TABLE Promotions(
-    id_promotion char(12) not NULL,
+    id_promotion char(36) not NULL,
     remise integer,
     date_debut datetime not null,
     date_fin datetime not null,
@@ -14,8 +25,8 @@ CREATE TABLE Promotions(
 );
 
 CREATE TABLE Appliquer(
-    id_promotion char(12),
-    id_produit char(12),
+    id_promotion char(36),
+    id_produit char(36),
     prix_remise double,
     PRIMARY KEY (id_promotion, id_produit),
     FOREIGN KEY (id_promotion)
@@ -27,3 +38,23 @@ CREATE TABLE Appliquer(
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
+
+CREATE TABLE Facturer(
+    id_client char(36),
+    id_produit char(36),
+    id_facture char(36) not null,
+    date_achat datetime not null,
+    quantite integer,
+    prix_total DOUBLE,
+    FOREIGN KEY (id_produit)
+    REFERENCES Produits(id_produit)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+    FOREIGN KEY (id_client)
+    REFERENCES Clients(id_client)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION
+);
+
+DROP TABLE Clients;
+DROP TABLE Facturer
