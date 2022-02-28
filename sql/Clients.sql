a54939a17a6feb3918a71c58907170c42d8c16bb
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2022-02-27 22:26:01 -0500

    entité produits

---
+++ sql/Clients.sql
@@ -0,0 +1,8 @@
+create table if not exists Clients (
+    idClient char(36) not null ,
+    nom varchar(20) not null ,
+    prenom varchar(20),
+    email varchar(20),
+    adresse varchar(50),
+    telephone char(11)
+);
