3c7753af5edcd98875f5621e1312484aeac7ec02
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2022-04-19 16:39:21 -0400

    dump evaluations

---
+++ sql/fillevaluation.py
@@ -0,0 +1,31 @@
+import random
+from datetime import datetime
+
+import lorem
+import pandas as pd
+import pymysql as sql
+
+host = "root"
+pwd = "13Loulou#"
+conn = sql.Connection(user="root", password=pwd,
+                      host="localhost", database="projet_glo_2005")
+conn.begin()
+cursor = conn.cursor()
+
+liste = []
+
+cursor.execute("select id_client from clients")
+clients = cursor.fetchall()
+
+cursor.execute('select isbn from livres limit 150')
+livres = cursor.fetchall()
+i = 0
+while i < 140:
+    client = random.choice(clients)
+    isbn = random.choice(livres)
+    if (client, isbn) not in liste:
+        liste.append((client,isbn))
+        i += 1
+        cursor.execute('insert into evaluer (id_client, isbn, note, commentaire, date) values(%s, %s, %s, %s, now())',
+                       (client, isbn, random.randint(0, 5), lorem.sentence()))
+        conn.commit()
